d8d69674427a65c985a2f81e42ed9d4b4376b234
Rene Fimbres <renefimbres@MacBook-Pro-de-Rene.local>
2026-05-06 14:02:09 -0700

    feat: add PWA support with service worker, manifest, icons and GitHub Pages deployment

---
+++ frontend/public/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'hsda-card-v1';
+
+const ASSETS = [
+  '/membresia-salud-hsda/',
+  '/membresia-salud-hsda/index.html',
+  '/membresia-salud-hsda/icon-192x192.png',
+  '/membresia-salud-hsda/icon-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      );
+    })
+  );
+});
